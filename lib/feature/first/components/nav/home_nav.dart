720a07963930451f2daedf80fed3c25c30674fbe
Marwan Atef <marwanatef.mahmoud@gmail.com>
2026-07-26 16:22:05 +0300

    last edit

--- lib/feature/first/components/nav/home_nav.dart
+++ lib/feature/first/components/nav/home_nav.dart
@@ -11,7 +11,7 @@ class HomeNav extends StatelessComponent {
   Component build(BuildContext context) {
     return div(
       classes:
-          "flex fixed top-0 left-0 right-0 px-4 sm:px-6 md:px-8 py-4 justify-between bg-[#09090B] z-[1000] overflow-hidden",
+          "flex fixed top-0 left-0 right-0 px-2 sm:px-4 md:px-6 lg:px-8 py-3 sm:py-4 justify-between bg-[#09090B] z-[1000] overflow-hidden",
       [
         const NavLeft(),
         div(classes: "hidden md:block", [
