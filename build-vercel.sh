997d6b81940717a44ff7cce0b50adc19f9120fe0
Marwan Atef <marwanatef.mahmoud@gmail.com>
2026-07-26 19:03:27 +0300

    vercel 2

--- build-vercel.sh
+++ build-vercel.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Install Dart SDK for Alpine Linux
-wget https://storage.googleapis.com/dart-archive/channels/stable/release/3.5.0/sdk/dartsdk-linux-x64-release.zip
-unzip dartsdk-linux-x64-release.zip
+# Install Dart SDK using curl
+curl -L https://storage.googleapis.com/dart-archive/channels/stable/release/3.5.0/sdk/dartsdk-linux-x64-release.zip -o dartsdk.zip
+unzip dartsdk.zip
 export PATH="$PATH:$PWD/dart-sdk/bin"
 
 # Install Tailwind CSS
-curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
-chmod +x tailwindcss-linux-x64
+curl -L https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64 -o tailwindcss
+chmod +x tailwindcss
 export PATH=$PATH:.
 
 # Install Dart dependencies
 dart pub get
 
 # Build CSS with Tailwind
-./tailwindcss-linux-x64 -i web/styles.tw.css -o web/styles.css --minify --config tailwind.config.js
+./tailwindcss -i web/styles.tw.css -o web/styles.css --minify --config tailwind.config.js
 
 # Activate Jaspr CLI
 dart pub global activate jaspr_cli
